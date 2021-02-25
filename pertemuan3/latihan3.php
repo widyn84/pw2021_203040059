e57e4750175f7b2fa2cf4acf2d876813095e68fe
widyn84 <widyn84@gmail.com>
2021-02-25 10:15:38 +0700

    Menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan3.php
@@ -0,0 +1,20 @@
+<?php
+// Pengkodisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20;
+if ($x < 20) {
+    echo "Benar";
+} 
+else if ($x == 20){
+    echo "Bingo!";
+} 
+else {
+    echo "Salah";
+}
+
+
+?>
