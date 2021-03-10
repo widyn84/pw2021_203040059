fb0727b746dd82d28d9a0121ac70edb212995895
widyn84 <widyn84@gmail.com>
2021-03-10 14:04:19 +0700

    Menyelesaikan video pertemuan 5

--- contoh/built-in.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo strlen("Widy Nugraha");
-echo "<br>";
-echo date("l, d - M - Y");
-echo "</br>";
-echo htmlspecialchars("<img src = 'img.jpg'>", ENT_NOQUOTES);
-?>
--- contoh/tulismiring.php
+++
@@ -1,7 +0,0 @@
-<?php
-function tulis_miring($text) {
-    echo "<i>$text</i>";
-}
-
-echo tulis_miring("Ini Miring!");
-?>
